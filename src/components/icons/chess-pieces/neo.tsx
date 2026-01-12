268a861acaac60d0fafdbe01a487d999a6292659
bhupalan subramaniyam <bhupalans@gmail.com>
2026-01-12 07:37:59 +0000

    please

--- src/components/icons/chess-pieces/neo.tsx
+++ src/components/icons/chess-pieces/neo.tsx
@@ -1,3 +1,4 @@
+
 import type { PieceColor, ChessPieceProps, PieceComponent } from "@/lib/types";
 
 const pieceStyles = {
@@ -13,8 +14,8 @@ const Pawns = ({ color }: { color: PieceColor }) => (
 
 const Rooks = ({ color }: { color: PieceColor }) => (
   <svg viewBox="0 0 45 45" style={pieceStyles[color]}>
-    <path d="M12 36V12h21v24H12z M14 14v10 M31 14v10 M12 12V9h21v3 M14 9h17" fill="none" />
-    <path d="M12 36h21v3H12v-3z" />
+    <path d="M12 36V12h21v24H12z M14 14v10 M31 14v10 M12 12V9h21v3" fill="none" />
+    <path d="M12 39h21v-3H12v3z M14 9h17" />
   </svg>
 );
 
@@ -22,29 +23,30 @@ const Knights = ({ color }: { color: PieceColor }) => (
   <svg viewBox="0 0 45 45" style={pieceStyles[color]}>
     <path d="M 15,36 C 15,36 15,28 18,26 C 21,24 22.5,24 22.5,24 C 22.5,24 26,22 26,18 C 26,14 24,10 22,10 C 20,10 19.5,12 19.5,12 C 19.5,12 21.5,10.5 22,8 C 22.5,5.5 18,4 18,4 C 18,4 12,10 12,14 C 12,18 15,20 15,20 C 15,20 12,23 12,26 C 12,29 15,36 15,36 z" />
     <path d="M 22.5,24 C 22.5,24 24,28 27,28 C 30,28 30,36 30,36" fill="none"/>
+    <path d="M 30 39 h -15 v -3" fill="none" />
   </svg>
 );
 
 const Bishops = ({ color }: { color: PieceColor }) => (
   <svg viewBox="0 0 45 45" style={pieceStyles[color]}>
-    <path d="M15 36h15l-7.5-9z" />
-    <path d="M22.5 9.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+    <path d="M15 39h15v-3H15v3z M22.5,9.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
     <path d="M18 14l-3 14h15l-3-14" fill="none" />
-    <path d="M22.5 28L15 14h15L22.5 28z" fill="none"/>
+    <path d="M15 14h15l-7.5 14L15 14z" />
+    <path d="M20.5 18.5L24.5 18.5" fill="none" />
   </svg>
 );
 
 const Queens = ({ color }: { color: PieceColor }) => (
   <svg viewBox="0 0 45 45" style={pieceStyles[color]}>
-    <path d="M12 36h21v-3H12v3z M13.5 33l-1.5-12h22l-1.5 12h-19z" />
-    <path d="M10 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM22.5 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM35 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0z" fill={pieceStyles[color].fill}/>
-    <path d="M12 21h21" fill="none"/>
+    <path d="M12 39h21v-3H12v3z M13.5 36l-1.5-12h22l-1.5 12h-19z" />
+    <path d="M10 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM22.5 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM35 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0z" />
+    <path d="M12 24h21" fill="none"/>
   </svg>
 );
 
 const Kings = ({ color }: { color: PieceColor }) => (
   <svg viewBox="0 0 45 45" style={pieceStyles[color]}>
-    <path d="M12 36h21v-3H12v3z M13.5 33l-1.5-12h22l-1.5 12h-19z M12 21h21" fill="none" />
+    <path d="M12 39h21v-3H12v3z M13.5 36l-1.5-12h22l-1.5 12h-19z M12 24h21" fill="none" />
     <path d="M22.5 6v10m-5-5h10" fill="none" />
   </svg>
 );
