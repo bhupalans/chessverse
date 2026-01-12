0b20174c0f3e3726d7470ef86034ae9a3b3506ee
bhupalan subramaniyam <bhupalans@gmail.com>
2026-01-12 15:13:04 +0000

    ADD CHESS CLOCKS
    Extend initializeLiveGame so that when a game becomes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -79,7 +79,30 @@ export const submitMove = functions.https.onCall(async (data, context) => {
             sound: sound,
         };
 
-        await gameRef.update({ fen: newFen, turn: newTurn, lastMove });
+        const updates: any = {
+            fen: newFen,
+            turn: newTurn,
+            lastMove: lastMove
+        };
+        
+        // Clock updates
+        if(gameData.clocks && firestoreGameData.timeControl) {
+            const now = Date.now();
+            const lastTick = gameData.clocks.lastTick;
+            const elapsed = (now - lastTick) / 1000; // in seconds
+            
+            const playerToUpdate = game.turn() === 'w' ? 'black' : 'white';
+            const increment = firestoreGameData.timeControl.increment || 0;
+            
+            let newTime = gameData.clocks[playerToUpdate] - elapsed + increment;
+            if (newTime < 0) newTime = 0;
+            
+            updates['clocks/lastTick'] = admin.database.ServerValue.TIMESTAMP;
+            updates[`clocks/${playerToUpdate}`] = newTime;
+        }
+
+
+        await gameRef.update(updates);
         
         if (game.isGameOver()) {
             let reason = 'Game Over';
@@ -142,11 +165,21 @@ export const initializeLiveGame = functions.firestore
         
         const startingFen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
         
-        console.log(`Initializing live game at /liveGames/${gameId}`);
-        return liveGameRef.set({
+        const liveGameState: any = {
             fen: startingFen,
             turn: 'w'
-        });
+        };
+
+        if (afterData.timeControl) {
+            liveGameState.clocks = {
+                white: afterData.timeControl.initial,
+                black: afterData.timeControl.initial,
+                lastTick: admin.database.ServerValue.TIMESTAMP
+            };
+        }
+
+        console.log(`Initializing live game at /liveGames/${gameId}`);
+        return liveGameRef.set(liveGameState);
     });
 
 export const handleGameAction = functions.https.onCall(async (data, context) => {
