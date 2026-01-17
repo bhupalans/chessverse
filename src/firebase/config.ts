9d89b967f5a3890e7ab4ea3bbab311294927e16b
bhupalans <72866845+bhupalans@users.noreply.github.com>
2026-01-17 16:21:34 +0530

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@
 export const firebaseConfig = {
   "projectId": "studio-1594147532-e6c2d",
   "appId": "1:756994608148:web:2ee9a9f5aa643ecf5cab01",
-  "apiKey": "AIzaSyC03m_yv3kcE5yeuGoDbFl8TBo0cOsn1LU",
+  "apiKey": "Enter the API here",
   "authDomain": "studio-1594147532-e6c2d.firebaseapp.com",
   "databaseURL": "https://studio-1594147532-e6c2d-default-rtdb.asia-southeast1.firebasedatabase.app",
   "measurementId": "",
